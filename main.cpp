5b8915fd4f62f35a272d196eb48cbac122211674
Bán Marcell <afga93+cc@gmail.com>
2017-09-15 20:09:25 +0200

    multiple greetings

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc, char *argv[]) {
     if (argc > 1)
-        std::cout << "Hello, " << argv[1] << "!" << std::endl;
+        for (int i = 1; i < argc; ++i)
+            std::cout << "Hello, " << argv[i] << "!" << std::endl;
     else
         std::cout << "Hello, World!" << std::endl;
     return 0;
